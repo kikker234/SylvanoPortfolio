63116c016f11ad6724c3dc55c60a8a601ae7cdf0
Justin9091 <jusraaijmakers@gmail.com>
2024-05-25 13:23:55 +0200

    Github action check

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,13 +9,12 @@ import PortfolioGrid from "@/Pages/Portfolio/PortfolioGrid.vue";
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">Preview</h2>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <h2 class="p-3 font-semibold text-3xl text-gray-800 leading-tight">Preview:</h2>
 
                     <PortfolioGrid />
                 </div>
