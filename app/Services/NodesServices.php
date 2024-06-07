cb4b83668232105e264e986fedc66b6cd6889c01
Justin9091 <jusraaijmakers@gmail.com>
2024-06-07 17:29:33 +0200

    Buurman & buurman oplossing

--- app/Services/NodesServices.php
+++ app/Services/NodesServices.php
@@ -10,14 +10,15 @@ class NodesServices
 {
     public function getAllNodes(): Collection
     {
-        $nodes = Node::all();
-
-        foreach ($nodes as $node) {
-            // image is stored as: images/h6hP3DyylqdyAz2Ed0abafBaqUEDEh37V5VRQ5uy.jpg
-            // needs to be accessible by Vue frontend
-            $node->image = Storage::url($node->image);
-        }
-
-        return $nodes;
+//        $nodes = Node::all();
+//
+//        foreach ($nodes as $node) {
+//            // image is stored as: images/h6hP3DyylqdyAz2Ed0abafBaqUEDEh37V5VRQ5uy.jpg
+//            // needs to be accessible by Vue frontend
+//            $node->image = Storage::url($node->image);
+//        }
+//
+//        return $nodes;
+        return [];
     }
 }
